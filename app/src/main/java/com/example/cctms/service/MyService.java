b56c38514d6c9aa34c7ba44e3eba00fd72b2bfab
mzGitH <37521264+mzGitH@users.noreply.github.com>
2019-11-26 11:29:37 +0800

    后台进程

--- app/src/main/java/com/example/cctms/service/MyService.java
+++ app/src/main/java/com/example/cctms/service/MyService.java
@@ -11,25 +11,35 @@ import android.location.LocationManager;
 import android.location.LocationProvider;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.Looper;
 import android.os.SystemClock;
 import android.util.Log;
 import android.widget.Toast;
-import com.example.cctms.util.PermissionUtils;
 
+import com.alibaba.fastjson.JSON;
+import com.example.cctms.util.JsonData;
+
+import java.io.IOException;
 import java.util.Date;
 import java.util.Timer;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MyService extends Service {
     private LocationManager mLocationManager;
-    private String[] permissions = {
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-    };
-    private static final int REQUEST_PERMISSION_CODE = 12;
 
     private int Time = 1000*3;//周期时间
-    private int anHour =1*1000;// 这是8小时的毫秒数 为了少消耗流量和电量，8小时自动更新一次
+    private int anHour =5*1000;// 这是8小时的毫秒数 为了少消耗流量和电量，8小时自动更新一次
     private Timer timer = new Timer();
+
+    String url="http://192.168.43.50:8080/AONT/user/location";
+    double latitude=0,longitude=0;
+
     @Override
     public IBinder onBind(Intent intent) {
         // TODO Auto-generated method stub
@@ -55,7 +65,7 @@ public class MyService extends Service {
         return super.onStartCommand(intent, flags, startId);
     }
 
-    private String startLocate() {
+    private void startLocate() {
         mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         boolean providerEnabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
         if (providerEnabled) { //GPS已开启
@@ -69,10 +79,10 @@ public class MyService extends Service {
              * 备注：参数2和3，如果参数3不为0，则以参数3为准；参数3为0，则通过时间来定时更新；两者为0，则随时刷新
              */
             mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, locationListener);
+            sendRequestWithOkHttp();
         } else {
             Toast.makeText(this, "请打开GPS", Toast.LENGTH_SHORT).show();
         }
-        return "";
     }
 
     private LocationListener locationListener = new LocationListener() {
@@ -84,6 +94,8 @@ public class MyService extends Service {
             Log.e("xyh", "经度：" + location.getLongitude());
             Log.e("xyh", "海拔：" + location.getAltitude());
             Log.e("xyh", "时间：" + new Date().toString());
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
         }
 
         @Override
@@ -114,5 +126,47 @@ public class MyService extends Service {
             Log.e("xyh", "onProviderDisabled: ");
         }
     };
+
+    private void sendRequestWithOkHttp() {
+        Toast.makeText(this,"纬度："+latitude+"\n经度："+longitude,Toast.LENGTH_SHORT).show();
+        OkHttpClient client = new OkHttpClient();//创建OkHttpClient对象。
+        FormBody.Builder formBody = new FormBody.Builder();//创建表单请求体
+        formBody.add("latitude",String.valueOf(latitude));//传递键值对参数
+        formBody.add("longitude",String.valueOf(longitude));
+        Request request = new Request.Builder()//创建Request 对象。
+                .url(url)
+                .post(formBody.build())//传递请求体
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if(response.isSuccessful()){//回调的方法执行在子线程。
+                    parseJSONWithGSON(response.body().string());
+                }
+            }});//此处省略回调方法。
+    }
+    private void parseJSONWithGSON(String jsonData) {
+        //使用Jastjson
+        JsonData res= JSON.parseObject(jsonData, JsonData.class);
+        if(res==null){
+            Looper.prepare();
+            Toast.makeText(this,"登陆失败，请重试",Toast.LENGTH_SHORT).show();
+            Looper.loop();
+        }
+        else{
+            if(res.code==0){
+                Looper.prepare();
+//                Toast.makeText(this,"登陆成功",Toast.LENGTH_SHORT).show();
+                Looper.loop();
+            }else{
+                Looper.prepare();
+                Toast.makeText(this,"登陆失败，请重试",Toast.LENGTH_SHORT).show();
+                Looper.loop();
+            }
+        }
+    }
 
 }
